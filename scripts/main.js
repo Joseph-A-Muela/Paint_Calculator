f1de73ee0012589c177117764a752aa5e258d8e1
Joseph-A-Muela <josephamuela@gmail.com>
2016-03-07 13:49:18 -0600

    Completed the paint calcultor assignment.

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var width = prompt('What is the width of the room in feet?');
+var length = prompt('What is the length of the romm in feet?');
+var answer = parseFloat(length*width);
+var area = Math.ceil(width * length / 350);
+
+console.log('You have '+answer+' square feet to paint.');
+console.log('You will need to purchase '+area+' gallons of paint.');
+
+document.write('<div>You have '+answer+' square feet to paint.</div>');
+document.write('<div>You will need to purchase '+area+' gallons of paint.</div>');
